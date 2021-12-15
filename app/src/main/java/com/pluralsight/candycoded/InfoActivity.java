5f469882500389517d8509609bfc9337e5036886
Moses Asiago <mozzeago@gmail.com>
2021-12-15 07:53:43 +0300

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -44,7 +44,7 @@ public class InfoActivity extends AppCompatActivity {
     // TODO - Task 3 - Launch the Phone Activity
     // ***
     public void createPhoneIntent(View view) {
-        Uri uri = Uri.parse("tel:+254703107408");
+        Uri uri = Uri.parse("tel:0123456789");
         Intent callIntent = new Intent(Intent.ACTION_DIAL);
         callIntent.setData(uri);
         startActivity(callIntent);
